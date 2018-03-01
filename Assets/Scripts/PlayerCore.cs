bca91365bc343aba756fb7ff30f4d024cc616b4c
tomo <tomo_sample@gamail.com>
2018-03-02 01:34:38 +0900

    プレイヤーのテスト作成

---
+++ Assets/Scripts/PlayerCore.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Ame
+{
+    public class PlayerCore : MonoBehaviour, IDamagable
+    {
+        public int hp = 1000;
+        public int speed = 3;
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        public void ApplyDamage(int damageValue)
+        {
+            hp -= damageValue;
+            if (hp <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
