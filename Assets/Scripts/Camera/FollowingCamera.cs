06eff9b0c7adce68d337ebc885875d7d914aa46e
tomo <tomo_sample@gamail.com>
2018-04-30 11:04:56 +0900

    対象を追跡するカメラ追加

---
+++ Assets/Scripts/Camera/FollowingCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    [Header("追跡するオブジェクト")]
+    public Transform target;
+
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        if (target == null)
+            return;
+        transform.position = target.position + CalculateOffset();
+
+        transform.LookAt(target);
+    }
+
+    Vector3 CalculateOffset()
+    {
+        return target.forward * offset.z + target.right * offset.x
+            + target.up * offset.y;
+    }
+}
