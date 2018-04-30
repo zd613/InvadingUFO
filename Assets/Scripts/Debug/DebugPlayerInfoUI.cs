7bc7a1cd1d85b2ae00520fa433e918cc99c54f6a
tomo <tomo_sample@gamail.com>
2018-04-30 22:47:30 +0900

    プレイヤーのhpを表示するようにした

---
+++ Assets/Scripts/Debug/DebugPlayerInfoUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugPlayerInfoUI : MonoBehaviour
+{
+    public GameObject player;
+
+    [Header("テキスト")]
+    public Text hpText;
+
+    Health health;
+    private void Awake()
+    {
+        health = player.GetComponent<Health>();
+
+        if (health != null)
+        {
+            health.OnDamageTaken += () => UpdateHpText();
+        }
+    }
+
+    private void Start()
+    {
+        if (health != null)
+        {
+            UpdateHpText();
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+
+    void UpdateHpText()
+    {
+        hpText.text = string.Format("hp:{0,4} / {1,4} ", health.hp, health.MaxHp);
+    }
+}
