a16b00b8b12023b1340480746447c1799f5e92f0
tomo <tomo_sample@gamail.com>
2018-04-30 11:11:36 +0900

    カメラの回転を滑らかにした

--- Assets/Scripts/Camera/FollowingCamera.cs
+++ Assets/Scripts/Camera/FollowingCamera.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class FollowingCamera : MonoBehaviour
 {
+    //https://answers.unity.com/questions/1329163/how-to-use-quaternionslerp-with-transformlookat.html
+
+
     [Header("追跡するオブジェクト")]
     public Transform target;
 
     public Vector3 offset;
+    public float rotationSpeed;//2.7f
 
     private void LateUpdate()
     {
@@ -15,7 +19,9 @@ public class FollowingCamera : MonoBehaviour
             return;
         transform.position = target.position + CalculateOffset();
 
-        transform.LookAt(target);
+        //ターゲットとカメラの向き
+        var lookRot = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Time.deltaTime);
     }
 
     Vector3 CalculateOffset()
