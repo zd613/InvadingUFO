39e375ace9bf343d91b36fb7e3d897b3ec703def
tomo <tomo_sample@gamail.com>
2018-04-30 12:08:23 +0900

    フォルダ移動とplaneのhealth作成

---
+++ Assets/Scripts/FlyingObject/Plane/PlaneCore.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Ame;
+
+public class PlaneCore : MonoBehaviour
+{
+    public AbstractInputProvider inputProvider;
+
+    //event Action Move;
+    //event Action<float, float> Rotate;
+
+
+    //[Space]
+    //public UfoMove ufoMove;
+    //public UfoRotation ufoRotation;
+    //public UfoAttack ufoAttack;
+
+    //private void Start()
+    //{
+    //    Move += ufoMove.Move;
+    //    Rotate += (pitch, yaw) => ufoRotation.Rotate(pitch, yaw);
+    //}
+
+    //private void Update()
+    //{
+    //    if (inputProvider.BulletAttack)
+    //    {
+    //        ufoAttack.Fire();
+    //    }
+    //}
+
+    //private void FixedUpdate()
+    //{
+    //    Rotate(inputProvider.PitchValue, inputProvider.YawValue);
+    //    if (Move != null)
+    //    {
+    //        Move();
+    //    }
+    //}
+
+}
