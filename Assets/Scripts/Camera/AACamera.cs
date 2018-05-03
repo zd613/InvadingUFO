4d67984d9e15c6e331f7c2b11e2bfac33a220709
tomo <tomo_sample@gamail.com>
2018-05-03 13:12:54 +0900

    カメラ作成

---
+++ Assets/Scripts/Camera/AACamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AACamera : MonoBehaviour
+{
+    [Header("targer")]
+    public Transform antiAircraftGun;
+    [Header("crosshair")]
+    public Transform aaTarget;
+
+    Vector3 offset;
+    public float movingSpeed = 2.7f;//
+    public float rotationSpeed = 2.7f;//2.7f
+
+    private void Awake()
+    {
+        offset = transform.position - antiAircraftGun.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        if (antiAircraftGun == null)
+            return;
+
+        var desiredPos = antiAircraftGun.position + CalculateOffset();
+        transform.position = Vector3.Slerp(transform.position, desiredPos, Time.deltaTime * movingSpeed);
+
+        //ターゲットとカメラの向き
+        var lookRot = Quaternion.LookRotation(aaTarget.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Time.deltaTime * rotationSpeed);
+    }
+
+    Vector3 CalculateOffset()
+    {
+        return antiAircraftGun.forward * offset.z + antiAircraftGun.right * offset.x
+            + antiAircraftGun.up * offset.y;
+    }
+}
