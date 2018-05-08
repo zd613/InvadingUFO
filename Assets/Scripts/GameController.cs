f000a0222511cf7be97242192b1680dd7577c575
tomo <tomo_sample@gamail.com>
2018-05-08 23:07:05 +0900

    プレハブ更新

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,7 +4,39 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public bool isPlaying;
 
 
-    
+    private void Awake()
+    {
+        isPlaying = true;
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            StopGame();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            PlayGame();
+        }
+    }
+
+    public void StopGame()
+    {
+        Time.timeScale = 0;
+        isPlaying = false;
+    }
+
+    public void PlayGame()
+    {
+        Time.timeScale = 1;
+        isPlaying = true;
+    }
+
+
 }
