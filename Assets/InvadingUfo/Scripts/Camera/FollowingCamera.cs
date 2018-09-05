8f7c1edcebf4151dd86bf02dc32064a93e359ca9
ame <25921047+zd613@users.noreply.github.com>
2018-09-05 17:23:07 +0900

    カメラを水平方向の回転だけ回転させてテスト中

--- Assets/InvadingUfo/Scripts/Camera/FollowingCamera.cs
+++ Assets/InvadingUfo/Scripts/Camera/FollowingCamera.cs
@@ -24,16 +24,16 @@ public class FollowingCamera : BaseCamera
         }
 
 
-        var desiredRot = Quaternion.LookRotation(target.position - transform.position, transform.up);
-        var ea = desiredRot.eulerAngles;
-        var desiredRot2 = Quaternion.Euler(ea);
-        var q = desiredRot * Quaternion.Euler(0, 0, 10);
-        var ea2 = desiredRot2.eulerAngles;
-        print(desiredRot);
-        print(desiredRot2);
+        //var desiredRot = Quaternion.LookRotation(target.position - transform.position, transform.up);
+        //var ea = desiredRot.eulerAngles;
+        //var desiredRot2 = Quaternion.Euler(ea);
+        //var q = desiredRot * Quaternion.Euler(0, 0, 10);
+        //var ea2 = desiredRot2.eulerAngles;
+        //print(desiredRot);
+        //print(desiredRot2);
 
-        print(ea);
-        print(ea2);
+        //print(ea);
+        //print(ea2);
     }
 
     protected override void LateUpdate()
@@ -54,24 +54,69 @@ public class FollowingCamera : BaseCamera
         //transform.position = desiredPos;
     }
 
+
+    //TODO:x と yだけ操作して　zは0で固定しておきたい
     protected override void Rotate()
     {
         Debug.DrawRay(transform.position, transform.forward * 2, Color.blue);
         Debug.DrawRay(transform.position, transform.up * 2, Color.green);
+
+
+        //y
+        var targetDir = target.position - transform.position;
+        var forward = transform.forward;
+        var angleY = Vector3.SignedAngle(forward, targetDir, Vector3.up);
+        print(angleY);
+
+        //ほぼターゲットを見てる
+        if (Mathf.Abs(angleY) < 2)
+        {
+            //transform.Rotate(Vector3.up, -angleY, Space.World);
+        }
+        //
+        else
+        {
+            float rotationValueY = 0;
+            if (angleY > 5)//カメラの右側にターゲット
+            {
+                rotationValueY = 1;
+            }
+            else if (angleY > 2)
+            {
+                rotationValueY = 0.01f;
+            }
+            else if (angleY < -5)
+            {
+                rotationValueY = -1;
+            }
+            else if (angleY < -2)
+            {
+                rotationValueY = -0.01f;
+            }
+            print(rotationValueY);
+
+            transform.Rotate(Vector3.up, rotationValueY * rotationSpeed * Time.deltaTime, Space.World);
+        }
+
+        //var angleX = Vector3.SignedAngle(forward, targetDir, transform.right);
+
+
+        //transform.Rotate(transform.right, angleX * Time.deltaTime);
+
+
+
+
+
+
+        return;
+
         //////上下さかさまになってしまうのを防ぐためtransform.up
-        var desiredRot = Quaternion.LookRotation(target.position - transform.position,transform.up);
-
-
-        //print(desiredRot.eulerAngles);
-        //var q = Quaternion.AngleAxis(-desiredRot.eulerAngles.z, transform.forward);
-        //desiredRot = desiredRot * q;
+        var desiredRot = Quaternion.LookRotation(target.position - transform.position, transform.up);
         var dr = desiredRot * Quaternion.AngleAxis(-desiredRot.eulerAngles.z, transform.forward);
-        //print(dr.eulerAngles);
-        ////TODO:zは回転させない ここおかしい
-        ////反転するようになる
-
         transform.rotation = desiredRot;
-        //transform.Rotate(new Vector3(0, 0, -transform.localEulerAngles.z));
+
+
+
     }
 
     Vector3 CalculateOffset()
