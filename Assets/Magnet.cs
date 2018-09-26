22fcd44d33a12d249f739d8a6144766bb3250eba
ame <25921047+zd613@users.noreply.github.com>
2018-09-26 18:23:20 +0900

    都市侵攻ステージ追加 家を引き寄せるようにしてる途中

---
+++ Assets/Magnet.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+    public float maxLength = 100;
+    public float power = 10;
+
+    private void Update()
+    {
+        Debug.DrawRay(transform.position, Vector3.down * maxLength);
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            Attract();
+        }
+        else
+        {
+            if (targetRigidbody != null)
+            {
+                targetRigidbody.useGravity = true;
+                targetRigidbody = null;
+            }
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            var move = GetComponent<Movement>();
+            move.isActive = !move.isActive;
+        }
+    }
+
+    GameObject target;
+    Rigidbody targetRigidbody;
+
+
+    //TODO
+    void Attract()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, maxDistance: maxLength))
+        {
+            if (!targetRigidbody)//null
+            {
+                targetRigidbody = hit.transform.GetComponent<Rigidbody>();
+                if (targetRigidbody == null)
+                    return;
+                targetRigidbody.useGravity = false;
+            }
+            else if (target != hit.transform.gameObject)//change target
+            {
+                targetRigidbody.useGravity = true;
+
+
+                targetRigidbody = hit.transform.GetComponent<Rigidbody>();
+                targetRigidbody.useGravity = false;
+            }
+
+            targetRigidbody.AddForce(Vector3.up * power);
+
+
+            //get target
+            if (Vector3.Distance(transform.position, hit.transform.position) < 20)
+            {
+                Destroy(hit.transform.gameObject);
+                targetRigidbody = null;
+            }
+        }
+
+    }
+
+
+
+}
