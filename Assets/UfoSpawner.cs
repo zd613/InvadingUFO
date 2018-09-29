1718f953f992f74786a7ccc38bac04ba3da9bb66
ame <25921047+zd613@users.noreply.github.com>
2018-09-29 23:52:24 +0900

    エリア外の山作成とufo spawner作成途中

---
+++ Assets/UfoSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UfoSpawner : MonoBehaviour
+{
+
+    public GameObject ufoPrefab;
+
+    private void Awake()
+    {
+        SpawnUfo();
+    }
+
+    void SpawnUfo()
+    {
+        var rot = Quaternion.identity;
+        var obj = Instantiate(ufoPrefab, transform.position, rot);
+    }
+
+}
