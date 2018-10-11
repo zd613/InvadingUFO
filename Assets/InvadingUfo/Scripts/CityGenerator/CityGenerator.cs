5061ff9a9f58c32fa7b815aba930672a179c7615
ame <25921047+zd613@users.noreply.github.com>
2018-10-11 21:57:56 +0900

    道路2x2の生成を作ってる途中

--- Assets/InvadingUfo/Scripts/CityGenerator/CityGenerator.cs
+++ Assets/InvadingUfo/Scripts/CityGenerator/CityGenerator.cs
@@ -17,30 +17,25 @@ public class CityGenerator : MonoBehaviour
 
     public int gridWidthCount = 10;
     public int gridHeightCount = 10;
-    //public GameObject cellGameObject;
 
     public Vector3 gridOffset;
 
-    const int defaultDeltaDistance = 10;
+    public static readonly int DefaultDeltaDistance = 10;
 
-    [Header("道路")]
-    public string roadParentName = "Roads";
-    public GameObject roadParentGameObject;
-
-    //あとで複数の道路に変える
-    public GameObject roadGameObject;
-
-    public int deltaWidth = 4;
-    public int deltaHeight = 4;
-
-    public Vector3 roadOffset;
-    List<GameObject> roads = new List<GameObject>();
 
 
     CityCellType[,] cellTypes;
 
+    RoadGenerator roadGenerator;
+
     private void Awake()
     {
+    }
+
+    private void Start()
+    {
+        roadGenerator = GetComponent<RoadGenerator>();
+        roadGenerator.Initialize(rootGameObject, cellTypes);
         Generate();
     }
 
@@ -61,7 +56,7 @@ public class CityGenerator : MonoBehaviour
             {
                 var plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
                 plane.transform.SetParent(gridGameObject.transform);
-                plane.transform.localPosition = new Vector3(w * defaultDeltaDistance, 0, h * defaultDeltaDistance) + gridOffset * defaultDeltaDistance;
+                plane.transform.localPosition = new Vector3(w * DefaultDeltaDistance, 0, h * DefaultDeltaDistance) + gridOffset * DefaultDeltaDistance;
                 grid[h, w] = plane;
                 cellTypes[h, w] = CityCellType.None;
             }
@@ -70,79 +65,6 @@ public class CityGenerator : MonoBehaviour
 
     //road generator
 
-    void MakeRoads()
-    {
-        if (roadParentGameObject == null)
-        {
-            roadParentGameObject = new GameObject(roadParentName);
-            roadParentGameObject.transform.SetParent(rootGameObject.transform);
-        }
-
-        //Transform pt = roadParentGameObject.transform;
-
-        var rh = Random.Range(0, grid.GetLength(0));
-        var rw = Random.Range(0, grid.GetLength(1));
-
-        var mat = grid[rh, rw].GetComponent<MeshRenderer>().material;
-        mat.color = Color.red;
-        grid[rh, rw].GetComponent<MeshRenderer>().material = mat;
-
-        var height = grid.GetLength(0);
-        var width = grid.GetLength(1);
-        CreateRoadWithCross(rw, rh);
-
-
-        //z方向移動 height
-        for (int h = 0; h < height; h++)
-        {
-            var diff = h - rh;
-            print(diff);
-            if ((Mathf.Abs(diff) % (deltaHeight + 1)) == 0)//差がdeltaHeight+1の倍数
-            {
-
-                CreateRoadWithCross(rw, h);
-            }
-        }
-
-
-        //x方向移動　width
-        for (int w = 0; w < width; w++)
-        {
-            var diff = w - rw;
-            if ((Mathf.Abs(diff) % (deltaWidth + 1)) == 0)//差がdeltaHeight+1の倍数
-            {
-
-                CreateRoadWithCross(w, rh);
-            }
-        }
-
-
-
-    }
-
-    void CreateRoadWithCross(int centerWidth, int centerHeight)
-    {
-        var height = grid.GetLength(0);
-        var width = grid.GetLength(1);
-
-        //クロス方向に作る
-        for (int h = 0; h < height; h++)
-        {
-            for (int w = 0; w < width; w++)
-            {
-                if (h == centerHeight || w == centerWidth)
-                {
-                    if (cellTypes[h, w] == CityCellType.Road)
-                        continue;
-                    var position = (new Vector3(w, 0, h) + gridOffset) * defaultDeltaDistance;
-                    var obj = Instantiate(roadGameObject, position, Quaternion.identity);
-
-                    obj.transform.SetParent(roadParentGameObject.transform);
-                    cellTypes[h, w] = CityCellType.Road;
-                }
-            }
-        }
-    }
 
     void Generate()
     {
@@ -156,12 +78,8 @@ public class CityGenerator : MonoBehaviour
 
         MakeGrid();
 
-        MakeRoads();
+        roadGenerator.Initialize(rootGameObject, cellTypes);
+        roadGenerator.MakeRoads();
     }
 
-    enum RoadDirection
-    {
-        X,
-        Z,
-    }
 }
---
+++ Assets/InvadingUfo/Scripts/CityGenerator/RoadGenerator.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadGenerator : MonoBehaviour
+{
+    [Header("設定")]
+    public GameObject rootGameObject;
+    public CityCellType[,] cellTypes;//grid のcellが何かの情報
+    //public Vector3 rootOffset;
+
+    [Header("道路")]
+    public string roadParentName = "Roads";
+    public GameObject roadParentGameObject;
+    public RoadGenerationMode roadGenerationMode;
+    public Vector3 roadOffset;//道全体を移動させる
+    //あとで複数の道路に変える
+    public GameObject roadGameObject;
+    public GameObject road2x2Crossroad;
+    public GameObject road2x2Straight;
+    public GameObject road2x2Edge;
+
+    public int deltaWidth = 4;
+    public int deltaHeight = 4;
+
+    //public Vector3 roadOffset;
+    List<GameObject> roads = new List<GameObject>();
+
+    public void Initialize(GameObject rootGameObject, CityCellType[,] cellTypes)
+    {
+        this.rootGameObject = rootGameObject;
+        this.cellTypes = cellTypes;
+    }
+
+    public void MakeRoads()
+    {
+        if (roadParentGameObject == null)
+        {
+            roadParentGameObject = new GameObject(roadParentName);
+            roadParentGameObject.transform.SetParent(rootGameObject.transform);
+        }
+
+
+        if (roadGenerationMode == RoadGenerationMode.Cell1x1)
+        {
+            //ベース位置
+            var rh = 0;
+            var rw = 0;
+
+
+            var height = cellTypes.GetLength(0);
+            var width = cellTypes.GetLength(1);
+
+
+            //x方向移動　width
+            for (int w = 0; w < width; w++)
+            {
+                var diff = w - rw;
+                if ((Mathf.Abs(diff) % (deltaWidth + 1)) == 0)//差がdeltaHeight+1の倍数
+                {
+
+                    CreateRoadWithCross(w, rh);
+                }
+            }
+            //z方向移動 height
+            for (int h = 0; h < height; h++)
+            {
+                var diff = h - rh;
+                if ((Mathf.Abs(diff) % (deltaHeight + 1)) == 0)//差がdeltaHeight+1の倍数
+                {
+
+                    CreateRoadWithCross(rw, h);
+                }
+            }
+
+        }
+        else if (roadGenerationMode == RoadGenerationMode.Cell2x2)
+        {
+            //2x2 の　セルの表現
+            int w0 = 0;//右
+            int w1 = 1;//左
+            int h0 = 0;//下
+            int h1 = 1;//上
+
+            var height = cellTypes.GetLength(0);
+            var width = cellTypes.GetLength(1);
+
+            var position = (new Vector3(0.5f, 0, 0.5f) + roadOffset) * CityGenerator.DefaultDeltaDistance;
+            //var obj = Instantiate(road2x2GameObject, position, Quaternion.identity);
+
+
+        }
+
+
+    }
+
+    void CreateRoadWithCross(int centerWidth, int centerHeight)
+    {
+        var height = cellTypes.GetLength(0);
+        var width = cellTypes.GetLength(1);
+
+        //クロス方向に作る
+        for (int h = 0; h < height; h++)
+        {
+            for (int w = 0; w < width; w++)
+            {
+                if (h == centerHeight || w == centerWidth)
+                {
+                    if (cellTypes[h, w] == CityCellType.Road)
+                        continue;
+                    var position = (new Vector3(w, 0, h) + roadOffset) * CityGenerator.DefaultDeltaDistance;
+                    var obj = Instantiate(roadGameObject, position, Quaternion.identity);
+
+                    obj.transform.SetParent(roadParentGameObject.transform);
+                    cellTypes[h, w] = CityCellType.Road;
+                }
+            }
+        }
+    }
+
+}
+
+
+public enum RoadGenerationMode
+{
+    Cell1x1,
+    Cell2x2,
+}
